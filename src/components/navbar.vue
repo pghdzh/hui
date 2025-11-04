f0d7b9111347e60b4923ff3dab355f17eea6b2bd
pghdzh <pgh@cool.com>
2025-11-04 16:00:10 +0800

    服务器

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -30,7 +30,7 @@ import { io } from "socket.io-client";
 
 const siteId = "hui";
 const onlineCount = ref<number | null>(null);
-const socket = io("http://1.94.189.79:3000", { query: { siteId } });
+const socket = io("http://36.150.237.25:3000", { query: { siteId } });
 
 const open = ref(false);
 const isScrolled = ref(false);
